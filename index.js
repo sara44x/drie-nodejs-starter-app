0579a57f6133830cb101d9b583988537612d20f6
sara44x <saly997@live.com>
2017-06-21 14:03:11 +0300

    Changed the msg

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Sara')
+  response.send('Hey')
 })
 
 app.listen(app.get('port'), function() {
